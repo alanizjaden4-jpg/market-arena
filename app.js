5dc291a0141a00fc71d04e676869e8f86a8b851d
alanizjaden4-jpg <alanizjaden4@gmail.com>
2025-11-15 12:40:27 -0600

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-function queueUp(){
-  document.getElementById("status").innerHTML = "Finding opponents...";
-  setTimeout(()=>{document.getElementById("status").innerHTML="Match Found! Simulating..."},1500);
-  setTimeout(()=>{document.getElementById("status").innerHTML="You won +$127!"},3500);
-}
